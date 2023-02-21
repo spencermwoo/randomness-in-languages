203ce6e25ac28fa5fc1483ed54e5f7f01eab4237
spencermwoo <spencermwoo>
2023-02-20 22:25:23 -0500

    bash is slow so just gen numbers instead of full workflow

--- sources/bash.sh
+++ sources/bash.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
 N=1000000
-X=10
-BATCH_SIZE=10000
-OUTPUT_FILE="batch_bash${X}_${N}"
+X=1000
+OUTPUT_FILE="bash_${X}_${N}"
 
-# Generate N random numbers between 1 and X
-nums=()
-while (( ${#nums[@]} <= N )); do
-  batch=()
-  for ((i=0; i<BATCH_SIZE; i++)); do
-    batch+=($RANDOM)
-  done
-  for ((i=0; i<BATCH_SIZE; i++)); do
-    num=$((1 + (batch[i] * X) / 32767))
-    nums+=($num)
-  done
+counts=()
+
+for ((i=0; i<N; i++)); do
+  r=$(( $RANDOM % X ));
+  (( counts[r]++ ))
 done
 
-# Write output to file
-echo "${nums[@]}" > "$OUTPUT_FILE"
+echo "${counts[@]}" > "$OUTPUT_FILE"

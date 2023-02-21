78d7086903edf23e205db8383c785dffa1a52a2e
spencermwoo <spencermwoo>
2023-02-20 22:24:54 -0500

    bash batch for faster

--- sources/bash.sh
+++ sources/bash.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 
-# Set the number of random numbers to generate and the upper bound for the numbers
-N=10
-X=100
+N=1000000
+X=10
+BATCH_SIZE=10000
+OUTPUT_FILE="batch_bash${X}_${N}"
 
 # Generate N random numbers between 1 and X
-numbers=($(shuf -i 1-$X -n $N))
-
-# Calculate the probability of each number
-counts=($(printf "%s\n" "${numbers[@]}" | sort -n | uniq -c | awk '{print $1}'))
-total=${#numbers[@]}
-probabilities=()
-for count in "${counts[@]}"; do
-    probabilities+=("$((count / total))")
+nums=()
+while (( ${#nums[@]} <= N )); do
+  batch=()
+  for ((i=0; i<BATCH_SIZE; i++)); do
+    batch+=($RANDOM)
+  done
+  for ((i=0; i<BATCH_SIZE; i++)); do
+    num=$((1 + (batch[i] * X) / 32767))
+    nums+=($num)
+  done
 done
 
-# Generate a file name based on the values of N and X
-file_name="bash_$N_$X"
-
-# Create the "outputs" directory if it does not exist
-# mkdir -p outputs
-
-# Write the probabilities to a file in the "outputs" directory
-for ((i = 0; i < $total; i++)); do
-    printf "%d:%f\n" "${numbers[$i]}" "${probabilities[$i]}" >> "../outputs/$file_name"
-done
+# Write output to file
+echo "${nums[@]}" > "$OUTPUT_FILE"
